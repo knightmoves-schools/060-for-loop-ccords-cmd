c35f72183bf7a8a0c0656f2c9930bc0142905a65
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-12-02 15:18:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let todos = ['first todo', 'second todo', 'third todo'];
           function markAsDone(todos) {
-            return         
+            return   "done -" + todos[0];      
           };
 
 document.getElementById('result').innerHTML = markAsDone(todos);
+
