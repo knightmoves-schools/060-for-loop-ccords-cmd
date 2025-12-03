b54e955bb460c49842408fd6dc230fa847b7c5f8
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-12-03 11:11:49 -0600

    Fix syntax errors in markAsDone function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let todos = ['first todo', 'second todo', 'third todo'];
-          function markAsDone(todos, let i = 0, i < 3) {
+          function markAsDone(todos, let i = 0, i < 3, i++) {
             return   todos+= i;      
           };
 
 document.getElementById('result').innerHTML = markAsDone(todos);
+
 
 
